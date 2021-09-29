ce8ac09251c634221345e26418dc4d305775ee0f
Yash Rathore <yashsatyanarayan@gmail.com>
2021-09-29 15:30:46 +0530

    inut and output

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var txtbutton= document.querySelector("#txt-button")
-var 
+var translateButton = document.querySelector("#button-translate");
+var textArea = document.querySelector("#text-area");
+var outputDiv = document.querySelector("#output");
+
+function clickHandler(){
+    outputDiv.innerText = "isfafoisdfnoisdn " +  textArea.value
+};
+translateButton.addEventListener("click", clickHandler  ); 
